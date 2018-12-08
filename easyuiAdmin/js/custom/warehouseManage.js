ddc7dbde7199d4da07788b7af480d3fd0be2a249
Fantasy <xcclml@163.com>
2018-12-08 21:01:36 +0800

    warehouseManage修改

---
+++ easyuiAdmin/js/custom/warehouseManage.js
@@ -0,0 +1,35 @@
+$(function () {//ready()文档加载后
+    $.ajaxSetup({
+        headers:{
+            uid:$.cookie('uid'),
+            token:$.cookie('jwt')
+        }
+    });
+    $("#warehouseManage").datagrid({//easyUi的数据网格
+        url:genAPI('/settings/storageList'),
+        method:'post',
+        fitColumns:true,
+        striped:true,
+        nowrap:true,
+        pagination:true,
+        rownumbers:true,
+        singleSelect:true,
+        fit:true,
+        loadFilter:function (data) {
+            return data.data
+        },
+        columns:[[
+            { field:'id',title:'仓库id',width:20},
+            { field:'code',title:'仓库号',width:100,hidden:true},
+            { field:'name',title:'仓库名称',width:100},
+            { field:'status',title:'状态',width:100,formatter: function(value,row,index){
+                    if (value=="1"){
+                        return "启用";
+                    } else {
+                        return "禁用";
+                    }
+                }}
+        ]],
+    })
+    }
+)
